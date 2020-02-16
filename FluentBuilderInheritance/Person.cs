b45a9765ce0a6dd37421832ac44e8afd88699750
kamogelo-r <kamo.ramokgadi@gmail.com>
2020-02-16 12:50:23 +0200

    Add Fluent Builder Inheritance

---
+++ FluentBuilderInheritance/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBuilderInheritance
+{
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+
+    public class PersonInfoBuilder
+    {
+        protected Person person = new Person();
+        public PersonInfoBuilder Called(string name)
+        {
+            person.Name = name;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonInfoBuilder //open-closed principle
+    {
+        public PersonJobBuilder WorksAsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+    }
+}
---
+++ FluentBuilderInheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBuilderInheritance
+{
+    /// <summary>
+    /// builders inheriting from other builders = no problem
+    /// unless you are using fluent interfaces. This is
+    /// mitigated using recursive generics
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new PersonJobBuilder();
+            builder.Called("Mark");
+
+            Console.ReadLine();
+        }
+    }
+}
